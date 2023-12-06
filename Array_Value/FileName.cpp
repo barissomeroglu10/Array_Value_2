164a014e5d251415edbe057a7bfb4c04fde31730
baris <baris@BARSOMEROLU>
2023-12-07 01:05:32 +0300

    Add project files.

---
+++ Array_Value/FileName.cpp
@@ -0,0 +1,40 @@
+/*
+In this code we will get value from user for index of array
+and write array on the screen.
+
+Coder: Barýþ Someroðlu
+Date: 21.11.23 / 02:00 a.m.
+*/
+
+
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+	int B[5], deger;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Please Enter Value for element of the array: ";
+		cin >> deger;
+
+		B[i] = deger;
+	}
+
+	cout << " " << endl;
+	cout << "Elements for the Array: ";
+
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << B[i] << " ";
+	}
+
+	cout << " " << endl;
+
+	return 0;
+}
